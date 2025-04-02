cc1efc91d415df30f46968d2cd27bfb51fc8c840
Yanatthan <6510685057@student.tu.ac.th>
2025-04-02 16:14:43 +0700

    ver1

--- TodoApp/Models/LoginViewModel.swift
+++
@@ -1,8 +0,0 @@
-//
-//  LoginViewModel.swift
-//  TodoApp
-//
-//  Created by นางสาวญาณัชทัฬห์ คงกระจ่าง on 2/4/2568 BE.
-//
-
-import Foundation
--- TodoApp/Views/HeaderView.swift
+++ TodoApp/Views/HeaderView.swift
@@ -8,11 +8,33 @@
 import SwiftUI
 
 struct HeaderView: View {
+    let title: String
+    let subtitle: String
+    let background: Color
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack{
+            Rectangle()
+                .foregroundColor(.orange)
+                .rotationEffect(Angle(degrees: -15))
+            VStack{
+                Text("Register")
+                    .font(.system(size: 45))
+                    .fontDesign(.rounded)
+                    .bold()
+                Text("Start organizing todos")
+                    .font(.system(size: 25))
+            }
+            .foregroundColor(.white)
+            .padding(.top,70)
+        }
+        .frame(width: UIScreen.main.bounds.width * 2, height: 300)
+        .offset(y:-150)
     }
 }
 
-#Preview {
-    HeaderView()
+#Preview{
+    HeaderView(title: "Title", subtitle: "subtitle", background: .pink)
 }
+    
+
